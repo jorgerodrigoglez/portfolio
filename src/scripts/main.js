85c1f9bdceac03d264e3417f7daecf6135e83ab5
Jorge Rodrigo Gonzalez <jorgerodrigoglez@gmail.com>
2015-12-26 02:00:26 +0100

    subiendo archivos

---
+++ src/scripts/main.js
@@ -0,0 +1,82 @@
+   $(document).ready (function(){
+
+        var mainPanel = document.getElementById('mainPanel');
+        var sidePanel = document.getElementById('sidePanel');
+
+        var removePanel = document.getElementById('removePanel');
+        var removeContactPanel = document.getElementById('removeContactPanel');
+
+        var mainContactPanel = document.getElementById('mainContactPanel');
+        var sideContactPanel = document.getElementById('sideContactPanel');
+
+        var btnCookies = document.getElementById('btnCookies');
+        var panelCookies = document.getElementById('panelCookies');
+
+    /*hammer*/
+
+        //var hammerPanel = new Hammer(mainPanel);
+        //var hammerSide = new Hammer(sidePanel);
+
+        //var hammerContactPanel = new Hammer(mainContactPanel);
+        //var hammerContactSide = new Hammer(sideContactPanel);
+
+        //var hammerBlock = new Hammer(btnBlock);
+
+
+      /* Navegador Mobile */
+        //hammerPanel.on('swiperight', onSwipe);
+        //hammerPanel.on('swiperight', onSwipeContact);
+        //hammerPanel.on('swiperight', offBlock);
+
+      /* Navegador Desktop */
+        mainPanel.addEventListener('click', onSwipe);
+        removePanel.addEventListener('click', removeSwipe);
+
+        mainContactPanel.addEventListener('click', onSwipeContact);
+        removeContactPanel.addEventListener('click', removeSwipeContact);
+
+        btnCookies.addEventListener('click', removeCookies);
+
+        function onSwipe() {
+            sidePanel.classList.add('open-chat-panel');
+
+        }
+
+        function removeSwipe() {
+            sidePanel.classList.remove('open-chat-panel');
+
+        }
+
+       function onSwipeContact() {
+            sidePanelContact.classList.add('open-contact-panel');
+
+        }
+
+        function removeSwipeContact(){
+            sidePanelContact.classList.remove('open-contact-panel');
+        }
+
+        function removeCookies() {
+            panelCookies.classList.toggle('remove-cookies-panel');
+      }
+
+    });
+
+    //TWITTER
+
+        window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+          t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+
+        t._e = [];
+        t.ready = function(f) {
+          t._e.push(f);
+        };
+
+        return t;
+      }(document, "script", "twitter-wjs"));
